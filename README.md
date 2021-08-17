4e7c06b05c34bd59650040372f45702f1a74323e
michaelhesch <michael.hesch@gmail.com>
2021-08-17 16:23:06 +0100

    Update map marker styling and popup detail

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # python-volcano-map
 
-A web map providing locations of volcanoes and population using Python and the Folium library.
+A web map providing locations of volcanoes and population using Python and the Folium and Pandas libraries.
+
+This mini project was created as a part of a Udemy course on Python called "The Python Mega Course".
+More information can be found [here](https://www.udemy.com/course/the-python-mega-course/)
--- app.py
+++ app.py
@@ -9,13 +9,24 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
 
+# function to assign marker color based on elevation
+def color_producer(elevation):
+    if elevation < 1500:
+        return 'green'
+    elif 1500 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+# iframe content to display in markers
 html= """
 Volcano name:<br>
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
-Height: %s m
+Height: %sm
 """
 # create map variable to hold folium map data
-map = folium.Map(location=[39.58589540888904, -119.87107331602643], zoom_start=5, tiles = "Stamen Terrain")
+map = folium.Map(location=[39.9299, -104.8962], zoom_start=5, tiles = "Stamen Terrain")
 # create a new feature group, holds multiple features
 # helps when adding layer control feature to map
 fg = folium.FeatureGroup(name="My Map")
@@ -23,10 +34,13 @@ fg = folium.FeatureGroup(name="My Map")
 # easily loop through coordinates to add multiple markers
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(
+    fg.add_child(folium.CircleMarker(
         location=[lt, ln], 
         popup=folium.Popup(iframe), 
-        icon = folium.Icon(color = "green")
+        radius=7,
+        fill_color=color_producer(el),
+        fill_opacity=0.7,
+        color=color_producer(el)
         ))
 
 
