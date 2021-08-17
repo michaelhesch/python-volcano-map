513e7e7dec53b574bb410038b6100fa16332fcbe
michaelhesch <michael.hesch@gmail.com>
2021-08-17 17:46:26 +0100

    Refactor map layers and add layer control

--- app.py
+++ app.py
@@ -27,14 +27,14 @@ Height: %sm
 """
 # create map variable to hold folium map data
 map = folium.Map(location=[39.9299, -104.8962], zoom_start=5, tiles = "Stamen Terrain")
-# create a new feature group, holds multiple features
+# create a new feature group for population layer
 # helps when adding layer control feature to map
-fg = folium.FeatureGroup(name="My Map")
+fgp = folium.FeatureGroup(name="Volcano Locations")
 # add elements to feature group object, such as markers
 # loop through coordinates, elevation and name to add markers
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(
+    fgp.add_child(folium.CircleMarker(
         location=[lt, ln], 
         popup=folium.Popup(iframe), 
         radius=7,
@@ -43,13 +43,19 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
         color=color_producer(el)
         ))
 
-
+# add new feature group for population map layer
+fgv = folium.FeatureGroup(name="Population Map")
 # import geojson data to add population map layer
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+fgv.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
     style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
     else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}
     ))
 
-map.add_child(fg)
+# add feature group to the map
+map.add_child(fgp)
+map.add_child(fgv)
+# add layer control to map to allow selection of layers
+# must be added after feature group is added to map
+map.add_child(folium.LayerControl())
 # generate map html file based on Map method inputs
 map.save("map.html")
