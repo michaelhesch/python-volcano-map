5a02c2231ee4b7cf31dcf403a27b7989ad9cce2c
michaelhesch <michael.hesch@gmail.com>
2021-08-17 17:08:22 +0100

    Add styling properties to population layer

--- app.py
+++ app.py
@@ -45,7 +45,10 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
 
 
 # import geojson data to add population map layer
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+    style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+    else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}
+    ))
 
 map.add_child(fg)
 # generate map html file based on Map method inputs
